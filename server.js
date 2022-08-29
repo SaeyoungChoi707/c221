2beac6fe4e24ebaf3aad475d916c8170814b2a07
SaeyoungChoi707 <63861341+SaeyoungChoi707@users.noreply.github.com>
2022-08-29 19:46:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+server.listen(process.env.PORT || 3030);
+var nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    port:465,
+    host:"smtp.gmail.com",
+    auth:{
+        user:'pandey.hema@gmail.com',
+        pass:'idardqmiktqarkgb'
+    },
+    secure: true
+})
